8c35293f4a11d77ae2654c349429d73b153b0758
LePireDesPire <111895227+LePireDesPire@users.noreply.github.com>
2022-12-28 06:07:52 +0100

    Update Nitro Generator.py

--- Nitro Generator.py
+++ Nitro Generator.py
@@ -5,19 +5,10 @@ loww = 'abcdefghijklmnopqrstuvwxyz'
 digg = '0123456789'
 
 numgen = input('Combien de codes Nitro veux-tu generer : ')
-up, low, dig, = True, True, True,
-all = ""
+lenght = 16
 
-if up:
-    all += upp
-if low:
-    all += loww
-if dig:
-    all += digg
+all = upp + loww + digg
 
-    lenght = 16
-    amout = numgen
-
-    for x in range(int(numgen)):
-        nitro = ''.join(random.sample(all, lenght))
-        print(nitro)
+for x in range(int(numgen)):
+    nitro = ''.join(random.sample(all, lenght))
+    print(nitro)
